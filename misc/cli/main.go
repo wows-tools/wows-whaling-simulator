b4a328661788440368881e68270a8d5b0e451e0f
kakwa <carpentier.pf@gmail.com>
2022-12-04 18:22:29 +0100

    adding argument parsing for cli tool

--- misc/cli/main.go
+++ misc/cli/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/kakwa/wows-whaling-simulator/lootbox"
 	"github.com/kakwa/wows-whaling-simulator/wows"
@@ -8,20 +9,30 @@ import (
 )
 
 func main() {
+	num := flag.Int("n", 10, "number of containers opened")
+	realmStr := flag.String("realm", "eu", "Wows realm (eu, na, asia)")
+	nick := flag.String("nick", "", "Nickname of the player")
+	flag.Parse()
+
 	api_key := os.Getenv("WG_API_KEY")
 	wowsApi := wows.NewWowsAPI(api_key)
-	err := wowsApi.FillShipMapping()
+	realm, err := wows.WowsRealm(*realmStr)
+	if err != nil {
+		fmt.Printf("Error Getting the realm %s\n", err.Error())
+		os.Exit(1)
+	}
+	err = wowsApi.FillShipMapping()
 	if err != nil {
 		fmt.Printf("Error Prefilling the Ship Mapping: %s\n", err.Error())
 		os.Exit(1)
 	}
-	players, err := wowsApi.SearchPlayer(wows.EURealm, "kakwa", "exact")
+	players, err := wowsApi.SearchPlayer(realm, *nick, "exact")
 	if err != nil {
 		fmt.Printf("Error Searching the player: %s\n", err.Error())
 		os.Exit(1)
 	}
 
-	ships, err := wowsApi.GetPlayerShips(wows.EURealm, *players[0].AccountId)
+	ships, err := wowsApi.GetPlayerShips(realm, *players[0].AccountId)
 	if err != nil {
 		fmt.Printf("Error getting the player's ships: %s\n", err.Error())
 		os.Exit(1)
@@ -40,7 +51,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	for i := 0; i < 120; i++ {
+	for i := 0; i < *num; i++ {
 		err := ws.Draw()
 		if err != nil {
 			fmt.Printf("Error Drawing item: %s\n", err.Error())
