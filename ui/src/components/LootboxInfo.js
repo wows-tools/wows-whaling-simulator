56c9c6080f8893f62481797431cf767b733f3ded
kakwa <carpentier.pf@gmail.com>
2022-12-09 04:05:08 +0100

    implement triggering of a whaling session
    Still to do: display the whaling data

--- ui/src/components/LootboxInfo.js
+++ ui/src/components/LootboxInfo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useEffect} from 'react';
 import axios from 'axios';
 import {Image} from '@adobe/react-spectrum';
 import {Link} from '@adobe/react-spectrum';
@@ -6,90 +7,160 @@ import {Text} from '@adobe/react-spectrum';
 import {Heading} from '@adobe/react-spectrum';
 import {View} from '@adobe/react-spectrum';
 import {Flex} from '@adobe/react-spectrum';
+import {Form} from '@adobe/react-spectrum';
 import {Divider} from '@adobe/react-spectrum';
 import {IllustratedMessage} from '@adobe/react-spectrum';
-import {ActionButton, Dialog, ButtonGroup, Button, DialogTrigger, Slider, Picker, Item, SearchField} from '@adobe/react-spectrum';
+import {ComboBox, ActionButton, AlertDialog, ButtonGroup, Button, DialogTrigger, Slider, Picker, Item, SearchField, DialogContainer, TextField} from '@adobe/react-spectrum';
 import {Content} from '@adobe/react-spectrum';
 import {Link as RouterLink} from 'react-router-dom';
 import {useParams} from 'react-router-dom';
 import NotFound from '@spectrum-icons/illustrations/NotFound';
 import Money from '@spectrum-icons/workflow/Money';
 import User from '@spectrum-icons/workflow/User';
+import {useAsyncList} from 'react-stately';
 
 import { API_ROOT } from '../api-config';
 
-function withParams(Component) {
-  return props => <Component {...props} params={useParams()} />;
+function WhaleBox(props) {
+  const [isOpen, setOpen] = React.useState(false);
+  const [whaling, setWhaling] = React.useState(false);
+  const [realm, setRealm] = React.useState("");
+  const [player, setPlayer] = React.useState("");
+  const [numlootbox, setNumlootbox] = React.useState(25);
+
+  let realmOptions = [
+    { id: "eu", name: 'EU' },
+    { id: "na", name: 'NA' },
+    { id: "asia", name: 'Asia' },
+  ];
+
+  let list = useAsyncList({
+    async load({ signal, cursor, filterText }) {
+      if (filterText.length < 3) {
+	return {
+	  items: []
+	}
+      }
+      let res = await fetch(
+	`${API_ROOT}/api/v1/realms/${realm}/players?nick_start=${filterText}`,
+	{ signal }
+      );
+      let json = await res.json();
+
+      return {
+	items: json.players
+      };
+    }
+  });
+
+  const triggerWhaling = () => {
+    axios.get(`${API_ROOT}/api/v1/lootboxes/${props.lootboxId}/realms/${realm}/players/${player}/simple_whaling_quantity?number_lootbox=${numlootbox}`) 
+      .then(res => {
+	const stats = res.data;
+	props.setStats(stats);
+      })
+      setPlayer("")
+      list.setFilterText("")
+  }
+
+
+  return (
+    <>
+    <ActionButton onPress={() => setOpen(true)}>	
+    <Money /><Text>Start Whaling!</Text>
+    </ActionButton>
+    <DialogContainer onDismiss={() => setOpen(false)}>
+    {isOpen &&
+      <AlertDialog
+      title="Let's Do Some Whaling"
+      variant="confirmation"
+      primaryActionLabel="Start Whaling"
+      cancelLabel="Cancel"
+      onPrimaryAction={triggerWhaling}
+      isPrimaryActionDisabled={player.length === 0}
+      >
+
+      <Form maxWidth="size-3600">
+      <Picker
+      label="Realm/Wows Server"
+      items={realmOptions}
+      onSelectionChange={(selected) => setRealm(selected)}
+      autoFocus="true"
+      >
+      {(item) => <Item>{item.name}</Item>}
+      </Picker>
+      <ComboBox
+      label="Player"
+      items={list.items}
+      inputValue={list.filterText}
+      onInputChange={list.setFilterText}
+      loadingState={list.loadingState}
+      isDisabled={realm.length === 0}
+      onSelectionChange={(selected) => setPlayer(selected)}
+      >
+      {(item) => <Item key={item.account_id}>{item.nickname}</Item>}
+      </ComboBox>
+
+      <Slider label="Containers" defaultValue="25" maxValue="1000" onChange={setNumlootbox}/>
+      </Form>
+
+      </AlertDialog>
+    }
+    </DialogContainer>
+    </>
+  )
 }
 
-class LootboxInfo extends React.Component {
-  	state = {
-    		lootbox: null
-  	}
 
-  componentDidMount() {
-    let { lootboxId } = this.props.params;
+function LootboxInfo() {
+  const [stats, setStats] = React.useState(false);
+  const [lootbox, setLootbox] = React.useState(false);
+  let { lootboxId } = useParams();
+
+  const [isOpen, setOpen] = React.useState(false);
+  useEffect(() => {
     axios.get(`${API_ROOT}/api/v1/lootboxes/${lootboxId}`)
       .then(res => {
-        const lootbox = res.data;
-	console.log(res.data);
-        this.setState({ lootbox });
+	const lootbox = res.data;
+	setLootbox(lootbox);
       })
+  },[lootboxId]);
+
+  const validateInput = () => {
+    return false
   }
+  let lootboxContent;
+  if (lootbox) {
+    lootboxContent = (
+      <IllustratedMessage>
+      <Image height="200px" objectFit="scale-down" src={API_ROOT + lootbox.img} alt={lootbox.name} />
+      <Content>{lootbox.name}</Content>
+      </IllustratedMessage>
 
-  render() { 
-	let lootboxContent;
-	if (this.state.lootbox) {
-		lootboxContent = (
-        	<IllustratedMessage>
-        	<Image height="200px" objectFit="scale-down" src={API_ROOT + this.state.lootbox.img} alt={this.state.lootbox.name} />
-        	<Content>{this.state.lootbox.name}</Content>
-        	</IllustratedMessage>
-
-		)
-	} else {
-		lootboxContent = (
-    		<IllustratedMessage>
-    		  <NotFound />
-    		  <Heading>No result</Heading>
-    		  <Content>Container found</Content>
-    		</IllustratedMessage>
-		)
-	}
-    return (
-      <Flex alignContent="center" justifyContent="center" margin="size-100" direction="column" gap="size-100" justifyContent="center" alignContent="center" alignItems="center">
-      <View backgroundColor="gray-200" borderColor="dark" borderRadius="small" width="size-2400">
-        {lootboxContent}
-      </View>
-      <View>
-      <DialogTrigger>
-      <ActionButton><Money /><Text>Start Whaling!</Text></ActionButton>
-      {(close) => (
-        <Dialog>
-          <Heading>Whaling Parameters</Heading>
-          <Divider />
-          <Content>Please, fill the whaling parameters:</Content>
-	  <Slider label="Number of Containers to open" defaultValue="12" maxValue="1000" />
-	  <Picker label="Realm/Wows Server">
-  <Item key="na">NA</Item>
-  <Item key="eu">EU</Item>
-  <Item key="asia">Asia</Item>
-</Picker>
-	  <SearchField label="Search for users" icon={<User />} />
-          <ButtonGroup>
-            <Button variant="secondary" onPress={close}>Cancel</Button>
-            <Button variant="accent" onPress={close} autoFocus>Whale</Button>
-          </ButtonGroup>
-        </Dialog>
-      )}
-      </DialogTrigger>
-
-      </View>
-      <Divider size="M" />
-	
-      </Flex>
+    )
+  } else {
+    lootboxContent = (
+      <IllustratedMessage>
+      <NotFound />
+      <Heading>No result</Heading>
+      <Content>Container found</Content>
+      </IllustratedMessage>
     )
   }
+  return (
+    <Flex margin="size-100" direction="column" gap="size-100" justifyContent="center" alignContent="center" alignItems="center">
+    <View backgroundColor="gray-200" borderColor="dark" borderRadius="small" width="size-2400">
+    {lootboxContent}
+    </View>
+    <View>
+    <WhaleBox lootboxId={lootboxId} setStats={setStats} />
+    </View>
+
+    <Divider size="M" />
+
+
+    </Flex>
+  )
 }
 
-export default withParams(LootboxInfo);
+export default LootboxInfo;
