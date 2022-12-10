bdf31b553a1ef8f483767c9eeffc7187363b6b86
kakwa <carpentier.pf@gmail.com>
2022-12-10 11:26:39 +0100

    tweak to error page

--- ui/src/components/Error.js
+++ ui/src/components/Error.js
@@ -13,22 +13,22 @@ import {
   IllustratedMessage,
   Image,
 } from "@adobe/react-spectrum";
-import {Link as RouterLink} from 'react-router-dom';
+import { Link as RouterLink } from "react-router-dom";
 import { useRouteError } from "react-router-dom";
 
 function Error(props) {
   const error = useRouteError();
   console.error(error);
   return (
-      <Flex
-        direction="column"
-        width="100%"
-        gap="size-100"
-        borderWidth="thin"
-        borderColor="dark"
-        height="100%"
-      >
-	<View
+    <Flex
+      direction="column"
+      width="100%"
+      gap="size-100"
+      borderWidth="thin"
+      borderColor="dark"
+      height="100%"
+    >
+      <View
         backgroundColor="gray-50"
         width="size-6000"
         height="size-6000"
@@ -38,30 +38,29 @@ function Error(props) {
         borderColor="dark"
         borderRadius="medium"
         padding="size-100"
-	>
-	<IllustratedMessage>
-	<Image src="/logo512.png" alt="Whale" />
-	<Header>Oops! Something went wrong</Header>
-	<Divider/>
-	<Content>
-	<Text>
-	  <section>
-	  <h2>Error</h2>
-	  <h1>Message</h1>
-	  {error.statusText || error.message}
-	  </section>
-	
-	  <h1>Whatever...</h1>
-	  <section>
-	<Link>
-  		<RouterLink to="/">Back to Container List</RouterLink>
-	</Link>
-	  </section>
-	</Text>
-	</Content>
-	</IllustratedMessage>
-	</View>
-     </Flex>
+      >
+        <IllustratedMessage>
+          <Image src="/logo512.png" alt="Whale" />
+          <Divider />
+          <Content>
+            <Text>
+              <h1>Oops! Something went wrong</h1>
+              <section>
+                <h2>Message</h2>
+                {error.statusText || error.message}
+              </section>
+
+              <section>
+                <h2>Whatever...</h2>
+                <Link>
+                  <RouterLink to="/">Back to Container List</RouterLink>
+                </Link>
+              </section>
+            </Text>
+          </Content>
+        </IllustratedMessage>
+      </View>
+    </Flex>
   );
 }
 
